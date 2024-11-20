cff39a430859a25a43331ccccc4ee2d228377b00
William Jardim <williamalvesjardim@gmail.com>
2024-11-20 16:14:15 -0300

    tocando pelo teclado

---
+++ gui/keyboard.js
@@ -0,0 +1,9 @@
+window.mapaTeclas = {
+    cima  : ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+
+    baixo : ['a', 's', 'd', 'f', 'h', 'j', 'k', 'l', 
+             'z', 'x', 'c', 'v', 'b', 'n',' m', ',', '.'],
+             
+    todasTeclas: [],
+    tecla2tecla: {}
+}
--- gui/teclas.js
+++ gui/teclas.js
@@ -1,3 +1,5 @@
+var nomeInstrumento = 'piano';
+
 //Teclas de cima
 document.getElementById('teclas-cima').innerHTML = ``;
 for( let i = 300 ; i < 500 ; i = i + 10 ){
@@ -10,19 +12,52 @@ for( let i = 300 ; i < 1000 ; i = i + 10 ){
     document.getElementById('teclas-baixo').innerHTML += `<div class="key white" data-convert="false" data-note="${i}"></div>`;
 }
 
+mapaTeclas.todasTeclas = mapaTeclas.cima.concat(mapaTeclas.baixo);
+
 // Adiciona ouvintes de clique aos botões das teclas
-document.querySelectorAll('.key').forEach(key => {
+mapaTeclas.tecla2tecla = {};
+document.querySelectorAll('.key').forEach( (key, keyIndex) => {
+
+    const aTecla = mapaTeclas.todasTeclas[keyIndex];
+
+    key.setAttribute('tecla-vinculada', aTecla);
+    mapaTeclas.tecla2tecla[aTecla] = key;
+
+    function tocarTecla(){
+        const note = key.getAttribute('data-note');
+        switch(nomeInstrumento){
+            case 'guitarra':
+                playGuitarraEletrica(Number(note), 0.4);
+                break;
+
+            case 'piano':
+                 playPiano(Number(note), 0.2);
+                 break;
+
+            default:
+                break;
+        }
+    }
 
     key.addEventListener('click', () => {
-        const note = key.getAttribute('data-note');
-
-        if( !key.getAttribute('data-convert') ){
-            playGuitarraEletrica(notes[note], 0.5);
-        
-        }else if( key.getAttribute('data-convert') == "false" )
-        {
-            playGuitarraEletrica(Number(note), 0.5);
-        }
+        tocarTecla();
     });
 
+});
+
+window.addEventListener('keydown', function(evento){
+    const nomeTecla = evento.code.replace('Key', '').toLowerCase();
+    if( mapaTeclas.tecla2tecla[nomeTecla] ){
+        mapaTeclas.tecla2tecla[nomeTecla].click();
+        mapaTeclas.tecla2tecla[nomeTecla].oldBackgroundColor =  mapaTeclas.tecla2tecla[nomeTecla].style.backgroundColor;
+        mapaTeclas.tecla2tecla[nomeTecla].style.backgroundColor = 'green'
+    }
+});
+
+window.addEventListener('keyup', function(evento){
+    const nomeTecla = evento.code.replace('Key', '').toLowerCase();
+    if( mapaTeclas.tecla2tecla[nomeTecla] ){
+        mapaTeclas.tecla2tecla[nomeTecla].click();
+        mapaTeclas.tecla2tecla[nomeTecla].style.backgroundColor = mapaTeclas.tecla2tecla[nomeTecla].oldBackgroundColor;
+    }
 });
